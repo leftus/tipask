05cb370b7ba2160179c667ef8c6c61b5f2f72023
leftus <1399301831@qq.com>
2017-06-16 19:35:06 +0800

    share

--- resources/views/themes/default/article/share.blade.php
+++ resources/views/themes/default/article/share.blade.php
@@ -22,6 +22,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content=" width=device-width, initial-scale=1,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title><?php echo $listData->title;?></title>
 	<link rel="stylesheet" href="/share/css/base.css">
 	<script type="text/javascript" src="/share/js/common.js"></script>
