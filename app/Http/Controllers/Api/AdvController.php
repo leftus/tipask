bc0f45001838fc85a1f1e02da0566b14183a8158
leftus <1399301831@qq.com>
2017-06-22 16:24:53 +0800

    qrcode

--- app/Http/Controllers/Api/AdvController.php
+++ app/Http/Controllers/Api/AdvController.php
@@ -76,6 +76,7 @@ class AdvController extends Controller
         return response()->json(array('code'=>4,'msg'=>'上传失败','data'=>$file->getError()));
       }
       $destPath = realpath(public_path('upload/'.$user_id.'/'.$date));
+      return response()->json(array('code'=>5,'msg'=>'保存失败','data'=>$destPath));
       if(!file_exists($destPath)){
         mkdir($destPath,0755,true);
       }
