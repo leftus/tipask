a3cbab9034bcf779ac82c654ace4551af6f793eb
leftus <1399301831@qq.com>
2017-06-29 12:33:20 +0800

    width

--- resources/views/themes/default/home/article_h5.blade.php
+++ resources/views/themes/default/home/article_h5.blade.php
@@ -25,6 +25,6 @@
 	var video_height = video_ojb.offsetHeight;
 	var video_width  = video_ojb.offsetWidth;
 	var window_width = window.screen.width;
-	video_ojb.style.width = '"'+window_width+'px"';
-	//console.log(window_width);
+	video_ojb[0].style.width = window_width+'px';
+	console.log(window_width);
 </script>
