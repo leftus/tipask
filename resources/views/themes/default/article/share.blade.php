ff143fa7e1fe9555b8590e2cbee30e0144be3066
leftus <1399301831@qq.com>
2017-06-29 10:30:24 +0800

    contact

--- resources/views/themes/default/article/share.blade.php
+++ resources/views/themes/default/article/share.blade.php
@@ -60,7 +60,7 @@
 	.rateprt1{width: 56%;}
 	.ratep1{font-size: 0.36rem;color:#252525;}
 	.ratep2{font-size: 0.3rem;color:#666;line-height:0.5rem;}
-	.ratep3{width:1.125rem;height: 0.55rem;text-align: center;margin-left: 0.4rem;margin-top: 0.1rem;}
+	.ratep3{width:1.125rem;height: 0.55rem;text-align: center;}
 	.ratesimg2{width:0.8625rem;height:0.3875rem; position: absolute;right:0.3rem;top:0;}
 	/*广告弹窗*/
 		.adshow{width: 100%;height: 100%;background-color: rgba(0,0,0,0.5);position: fixed;top:0;left:0;z-index: 99;}
@@ -75,10 +75,9 @@
 		.adtel{margin-top: 0.75rem;}
 		.adtel p{margin-right: 0.5rem;}
 		.guanbi{position: absolute;bottom:15%;width: 0.8rem;left:50%;margin-left: -0.4rem;height:0.8rem;}
-		.top_tel{margin-top: 0.6rem}
 		.ad-bottom{margin-bottom:0.5rem;}
 		.contact{
-
+			width:1.125rem;
 		}
 	</style>
 </head>
@@ -98,7 +97,7 @@
 	 			<a href="<?php echo $advert->jump_url?>"><p class="ratep2"><?php echo $advert->descri;?><?php if($advert->jump_url):?><span style="color: #1792e8;">查看详情>></span><?php endif;?></p></a>
 	 		</div>
 	 		<div class="fl contact">
-	 		<p class="fl ratep3 top_tel"><a href="tel:<?php echo $advert->tel;?>"><img src="/share/images/diandian.png"></a></p>
+	 		<p class="fl ratep3"><a href="tel:<?php echo $advert->tel;?>"><img src="/share/images/diandian.png"></a></p>
 	 		<p class="fl ratep3">
 			<?php if($advert->qrcode):?>
 			<a href="<?php echo url('qrcode?qrcode='.urlencode($advert->qrcode));?>"><img src="/share/images/zizi.png" alt=""></a>
