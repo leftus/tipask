bcbe3c0b23c5e094f45bf23bf9ae13f8a04cd2e2
leftus <1399301831@qq.com>
2017-07-05 15:46:53 +0800

    toggle

--- app/Http/Controllers/Api/CategorieController.php
+++ app/Http/Controllers/Api/CategorieController.php
@@ -60,7 +60,7 @@ class CategorieController extends Controller
       }
       //var_dump($all_category);
       //var_dump($all_category_ids);
-      $user_category_ids = UserCategory::where('uid','=',$user_id)->pluck('category_id');
+      $user_category_ids = UserCategory::where('uid','=',$user_id)->pluck('category_id')->toArray();
       if(empty($user_category_ids)){
         $user_category_ids=array();
       }
