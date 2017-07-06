adb92f7b57947a30b48ff9858f221d2880a8ca1e
leftus <1399301831@qq.com>
2017-07-06 16:50:18 +0800

    download

--- resources/views/themes/default/article/share.blade.php
+++ resources/views/themes/default/article/share.blade.php
@@ -266,6 +266,8 @@ var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 $('.download').click(function(){
 	if(isAndroid){
 		location.href="http://zhushou.360.cn/detail/index/soft_id/3862371?recrefer=SE_D_";
+	}else{
+		location.href="https://itunes.apple.com/cn/app/%E5%8D%A1%E5%8D%A1%E8%90%A5%E9%94%80/id1244664884?mt=8";
 	}
 	if(isiOS){
 		location.href="https://itunes.apple.com/cn/app/%E5%8D%A1%E5%8D%A1%E8%90%A5%E9%94%80/id1244664884?mt=8";
