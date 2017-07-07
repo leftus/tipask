a2a720a186acaa8d8824b7a4026709c0afeb4a72
leftus <1399301831@qq.com>
2017-07-07 13:57:23 +0800

    shiyong

---
+++ resources/views/themes/default/home/introduction.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content=" width=device-width, initial-scale=1,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title>使用说明</title>
+	<style type="text/css">
+  body{
+    background: #f5f5f5;
+  }
+  .qrcode{
+    text-align: center;
+    margin:50% auto;
+    width:300px;
+  }
+  .qrcode img{
+    width:100%;
+  }
+	</style>
+</head>
+<body>
+  <div class="qrcode">
+    <p>使用说明</p>
+  </div>
+</body>
+</html>
