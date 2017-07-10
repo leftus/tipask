12a718ec6c15eca5517fce7d50eb9ccc05ff7c47
leftus <1399301831@qq.com>
2017-07-10 17:01:41 +0800

    msg

--- app/Http/Controllers/Api/MsgController.php
+++ app/Http/Controllers/Api/MsgController.php
@@ -88,8 +88,13 @@ class MsgController extends Controller
 			return 'error';
 		}
     $cate_list = Category::where('type','articles')->where('status','<>',0)->lists('id');
-		$article = Article::select('id','title','logo','content')->where('is_send','<>',1)->whereIn('category_id',$cate_list)->orderBy('id','desc')->first();
+		$article = Article::select('id','title','logo','content','category_id','summary')->where('is_send','<>',1)->whereIn('category_id',$cate_list)->orderBy('id','desc')->first();
 		if($article){
+			if($article->category_id==9){
+				$article->title = trim($article->summary);
+			}
+			unset($article->category_id);
+			unset($article->summary);
 			$article->desc    = str_limit($this->format_html($article->content), $limit = 40, $end = '');
 	    if(strpos($article->logo,'http')===FALSE){
 			    $article->logo    = 'https://us.m9n.com/image/show'.$article->logo;
